b420b368e8a2a53d18aeb9955256e7c7a9554426
Eric Pascual <eric@pobot.org>
2016-06-12 15:44:46 +0200

    replaces menus by selectors

--- src/pybot/youpi2/toplevel.py
+++ src/pybot/youpi2/toplevel.py
@@ -1,61 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import time
 import subprocess
-from inspect import isfunction
+import time
 
 from pybot.raspi import i2c_bus
-
+from pybot.youpi2.ctlpanel import Menu, Selector
 from .__version__ import version
 from .ctlpanel import ControlPanel
 
 __author__ = 'Eric Pascual'
 
 
-class Menu(object):
-    MENU_POSITIONS = {
-        ControlPanel.Keys.TL: (1, 1),
-        ControlPanel.Keys.BL: (4, 1),
-        ControlPanel.Keys.TR: (1, 20),
-        ControlPanel.Keys.BR: (4, 20),
-    }
-
-    BACK = -1
-
-    def __init__(self, title, choices, panel):
-        self.title = title
-        self.choices = choices
-        self.panel = panel
-
-    def display(self):
-        self.panel.clear()
-        self.panel.set_leds()
-        self.panel.center_text_at(self.title, 2)
-        self.panel.center_text_at('-' * len(self.title), 3)
-
-        for key, entry in self.choices.iteritems():
-            line, col = self.MENU_POSITIONS[key]
-            label = entry[0]
-            if col == 1:
-                self.panel.write_at('<' + label, line, col)
-            else:
-                s = label + '>'
-                self.panel.write_at(s, line, col - len(s) + 1)
-        self.panel.set_leds(self.choices.keys())
-
-    def get_and_process_input(self):
-        key = self.panel.wait_for_key(valid=self.choices.keys())
-        self.panel.leds_off()
-        action = self.choices[key][1]
-        return action() if isfunction(action) else action
-
-
 class TopLevel(object):
-    TERMINATE = -1
+    TERMINATE = -9
 
     def __init__(self):
         self.pnl = ControlPanel(i2c_bus)
-        self.terminated = False
 
     def display_about(self):
         self.pnl.display_splash("""
@@ -73,22 +33,37 @@ class TopLevel(object):
         menu = Menu(
             title='Main menu',
             choices={
-                ControlPanel.Keys.TL: ('Demo', self.demo_auto),
-                ControlPanel.Keys.TR: ('WS mode', self.web_service),
-                ControlPanel.Keys.BL: ('Man. ctrl', self.manual_control),
-                ControlPanel.Keys.BR: ('Tools', self.tools),
+                # ControlPanel.Keys.TL: ('Demo', self.demo_auto),
+                # ControlPanel.Keys.TR: ('WS mode', self.web_service),
+                # ControlPanel.Keys.BL: ('Man. ctrl', self.manual_control),
+                # ControlPanel.Keys.BR: ('Tools', self.tools),
+                ControlPanel.Keys.BL: ('System', self.tools),
+                ControlPanel.Keys.BR: ('Mode', self.mode_selector),
             },
             panel=self.pnl
         )
 
-        terminated = False
-        while not terminated:
+        while True:
             menu.display()
-            terminated = menu.get_and_process_input() == self.TERMINATE
+            if menu.handle_choice() == self.TERMINATE:
+                break
 
         self.pnl.set_backlight(False)
         self.pnl.clear()
-        self.pnl.set_leds()
+        self.pnl.leds_off()
+
+    def mode_selector(self):
+        sel = Selector(
+            title='Mode selection',
+            choices=(
+                ('Demo', self.demo_auto),
+                ('Manual', self.manual_control),
+                ('Network', self.web_service),
+            ),
+            panel=self.pnl
+        )
+        sel.display()
+        return sel.handle_choice()
 
     def demo_auto(self):
         self.pnl.display_splash("""
@@ -97,7 +72,7 @@ class TopLevel(object):
         Hit a key to end
         """, delay=0)
 
-        sequence = self.pnl.Keys.ALL
+        sequence = (self.pnl.Keys.TL, self.pnl.Keys.TR, self.pnl.Keys.BR, self.pnl.Keys.BL)
         progress = 0
 
         clock = 0
@@ -110,15 +85,25 @@ class TopLevel(object):
             time.sleep(0.1)
 
             now = time.time()
-            if now - clock >= .5:
+            if now - clock >= .2:
                 progress = (progress + 1) % len(sequence)
                 self.pnl.set_leds(sequence[progress])
                 clock = now
 
-        self.pnl.set_leds()
+        self.pnl.leds_off()
 
     def web_service(self):
-        pass
+        self.pnl.display_splash("""
+        Web Services mode
+
+        Stop combo to end
+        """, delay=0)
+
+        while True:
+            keys = self.pnl.get_keys()
+            if keys == {self.pnl.Keys.BL, self.pnl.Keys.BR}:
+                break
+            time.sleep(0.1)
 
     def manual_control(self):
         subprocess.call(['top'])
@@ -127,18 +112,19 @@ class TopLevel(object):
         menu = Menu(
             title='Tools',
             choices={
-                ControlPanel.Keys.TL: ('Reset', self.reset_youpi),
-                ControlPanel.Keys.TR: ('Shutdown', self.shutdown),
-                ControlPanel.Keys.BL: ('About', self.display_about_modal),
-                ControlPanel.Keys.BR: ('Back', Menu.BACK)
+                ControlPanel.Keys.TL: ('Esc', Menu.BACK),
+                ControlPanel.Keys.TR: ('About', self.display_about_modal),
+                ControlPanel.Keys.BL: ('Reset', self.reset_youpi),
+                ControlPanel.Keys.BR: ('Shutdown', self.shutdown),
             },
             panel=self.pnl
         )
 
-        done = False
-        while not done:
+        while True:
             menu.display()
-            done = menu.get_and_process_input() == Menu.BACK
+            gpi = menu.handle_choice()
+            if gpi in (Menu.BACK, self.TERMINATE):
+                return gpi
 
     def display_about_modal(self):
         self.display_about()
@@ -153,18 +139,19 @@ class TopLevel(object):
         panel = Menu(
             title='Shutdown',
             choices={
-                ControlPanel.Keys.TL: ('Quit', 'Q'),
-                ControlPanel.Keys.TR: ('Reboot', 'R'),
-                ControlPanel.Keys.BL: ('Power off', 'P'),
-                ControlPanel.Keys.BR: ('Back', Menu.BACK)
+                ControlPanel.Keys.TL: ('Esc', Menu.BACK),
+                ControlPanel.Keys.TR: ('Quit', 'Q'),
+                ControlPanel.Keys.BL: ('Reboot', 'R'),
+                ControlPanel.Keys.BR: ('Power off', 'P'),
             },
             panel=self.pnl
         )
 
         panel.display()
-        action = panel.get_and_process_input()
+        action = panel.handle_choice()
         if action == Menu.BACK:
-            return
+            return Menu.BACK
+
         elif action == 'R':
             self.pnl.display_progress("Reboot")
             subprocess.call('sudo reboot', shell=True)
@@ -172,7 +159,7 @@ class TopLevel(object):
             self.pnl.display_progress("Shutdown")
             subprocess.call('sudo poweroff', shell=True)
 
-        self.terminated = True
+        return self.TERMINATE
 
 
 def main():
