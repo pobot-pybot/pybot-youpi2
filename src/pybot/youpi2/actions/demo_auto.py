80a3265099a7d0bbfe98173cef900a246f39f2d2
Eric Pascual <eric@pobot.org>
2016-06-19 23:28:38 +0200

    auto demo updated

--- src/pybot/youpi2/actions/demo_auto.py
+++ src/pybot/youpi2/actions/demo_auto.py
@@ -8,23 +8,36 @@ __author__ = 'Eric Pascual'
 
 
 class StandAloneDemo(Action):
-    def execute(self):
-        self.panel.display_splash("""
-        Automatic demo
+    def prepare(self):
+        # TODO self.arm.disable()
 
-        Hit a key to end
-        """, delay=0)
+        self.panel.display_splash(
+            """Place arm in neutral
+            position and hit
+            any key.
+            """, delay=0)
+
+    def execute(self):
+        self.prepare()
+
+        self.panel.display_splash("""
+        Automatic demo mode
+        """, delay=-1)
+
+        self.panel.clear_was_locked_status()
 
         sequence = (self.panel.Keys.TL, self.panel.Keys.TR, self.panel.Keys.BR, self.panel.Keys.BL)
         progress = 0
 
         clock = 0
-        self.panel.set_leds(sequence[progress])
 
+        self.panel.set_leds(sequence[progress])
         while True:
-            keys = self.panel.get_keys()
-            if keys:
-                break
+            self.panel.any_key_to_exit_message()
+
+            if self.panel.get_keys():
+                return
+
             time.sleep(0.1)
 
             now = time.time()
@@ -32,6 +45,3 @@ class StandAloneDemo(Action):
                 progress = (progress + 1) % len(sequence)
                 self.panel.set_leds(sequence[progress])
                 clock = now
-
-        self.panel.leds_off()
-
