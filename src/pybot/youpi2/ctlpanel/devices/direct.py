92b38943e0cdf3e53b9300e300a111b4aaa0d25c
Eric Pascual <eric@pobot.org>
2016-07-23 12:00:04 +0200

    code restructured

---
+++ src/pybot/youpi2/ctlpanel/devices/direct.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+""" This module provides classes for managing Youpi's control panel.
+
+The control panel is composed of a LCD05 4x20 LCD display, associated to
+a reduced matrix keypad (2x2 keys). LEDs are integrated in the key buttons
+and can be controlled by software.
+
+Both the LCD and the LEDs use I2C to communicate, the LEDs being driven
+by an I2C IO expander (PCF8574).
+
+The version of the :py:class:`ControlPanel` class defined in this module
+uses a direct access to the panel via the I2C bus.
+"""
+
+from pybot.lcd.lcd_i2c import LCD05
+from .keys import Keys
+
+__author__ = 'Eric Pascual'
+
+
+class ControlPanelDevice(LCD05):
+    """ Low level interface of the control panel device, using direct I2C bus access.
+
+    It provides also a modified view of the keypad, replacing the identification
+    of the keys by the digits printed on their faces by their position (top-left,
+    top-right,...).
+
+    It is implemented as an extended version of the LCD05 driver class, adding methods for
+    control the key LEDs and checking the lock switch.
+    """
+
+    EXPANDER_ADDR = 0x20
+
+    def __init__(self, bus, debug=False):
+        super(LCD05, self).__init__(bus, debug=debug)
+        self.was_locked = False
+
+    def get_leds_state(self):
+        port_state = self._bus.read_byte(self.EXPANDER_ADDR)
+        return ~port_state & 0x0f
+
+    def set_leds_state(self, state):
+        # Remember we work in inverted logic at expander level, since connected
+        # in sink mode => invert the LED states to obtain the port ones.
+        port_state = ~state
+        # Take care also to set other IOs as inputs (0xf0 mask)
+        self._bus.write_byte(self.EXPANDER_ADDR, port_state | 0xf0)
+
+    def set_leds(self, keys=None):
+        """ Turns a set of LEDs on.
+
+        .. seealso:: :py:meth:`Keys.mask` for parameter definition.
+        """
+        self.set_leds_state(Keys.mask(keys))
+
+    def leds_off(self):
+        """ Convenience function for turning all the LEDs off. """
+        self.set_leds_state(0)
+
+    def is_locked(self):
+        """ Tells if the lock switch is on or off.
+
+        The lock switch must be connected to P7 PCF IO, and must pull the input
+        down to the ground when closed. Since the security switch used here is opened
+        when the key removal position, the HIGH state corresponds to "locked".
+        """
+        return bool(self._bus.read_byte(self.EXPANDER_ADDR) & 0x80)
+
+    def reset(self):
+        """ Resets the panel by chaining the following operations :
+
+        * clear the display
+        * turns the back light on
+        * turns the cursor display off
+        * turns all the keypad LEDs off
+        * set the keypad scanner of the LCD controller in fast mode
+        """
+        self.leds_off()
+        super(ControlPanelDevice, self).reset()
---
+++ src/pybot/youpi2/ctlpanel/devices/fs.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+""" This module provides classes for managing Youpi's control panel.
+
+The version of the :py:class:`ControlPanel` class defined in this module
+offers functionality similar as the one in the `direct` sibling module,
+but works with the virtual file system interface instead of the I2C
+direct access.
+
+It thus requires that the `lcdfs` FUSE file system is available on the
+system.
+"""
+
+import os
+
+__author__ = 'Eric Pascual'
+
+
+class ControlPanelDevice(object):
+    F_BACKLIGHT = 'backlight'
+    F_BRIGHTNESS = 'brightness'
+    F_KEYS = 'keys'
+    F_DISPLAY = 'display'
+    F_INFO = 'info'
+    F_CONTRAST = 'contrast'
+    F_LEDS = 'leds'
+    F_LOCKED = 'locked'
+
+    _READ_WRITE = {
+        F_BACKLIGHT: True,
+        F_KEYS: False,
+        F_DISPLAY: True,
+        F_INFO: False,
+        F_CONTRAST: True,
+        F_BRIGHTNESS: True,
+        F_LEDS: True,
+        F_LOCKED: False,
+    }
+
+    KEYPAD_SCAN_PERIOD = 0.1
+
+    def __init__(self, mount_point, debug=False):
+        """
+        :param str mount_point: the path of the mount point where the panel FUSE file system is mounted
+        :param bool debug: activates the debug mode
+        """
+        if not os.path.isdir(mount_point):
+            raise ValueError('mount point not found : %s' % mount_point)
+
+        self._debug = debug
+
+        self.was_locked = False
+
+        self._mount_point = mount_point
+        self._fs_files = {
+            n: open(os.path.join(mount_point, n), 'r+' if self._READ_WRITE[n] else 'r')
+            for n in os.listdir(mount_point)
+        }
+
+        self._info = {}
+        for line in self._fs_files[self.F_INFO]:
+            attr, value = (j.strip() for j in line.split(':'))
+            try:
+                value = int(value)
+            except ValueError:
+                pass
+            self._info[attr] = value
+        self._width = self._info['cols']
+        self._height = self._info['rows']
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    def _fp(self, name):
+        try:
+            fp = self._fs_files[name]
+            fp.seek(0)
+            return fp
+        except KeyError:
+            raise TypeError("not supported")
+
+    def get_leds_state(self):
+        return int(self._fp(self.F_LEDS).read())
+
+    def set_leds_state(self, state):
+        fp = self._fp(self.F_LEDS)
+        fp.write(str(state))
+        fp.flush()
+
+    def is_locked(self):
+        """ Tells if the lock switch is on or off.
+        """
+        return bool(int(self._fp(self.F_LOCKED).read()))
+
+    def get_backlight(self):
+        return bool(int(self._fp(self.F_BACKLIGHT).read()))
+
+    def set_backlight(self, on):
+        fp = self._fp(self.F_BACKLIGHT)
+        fp.write('1' if on else '0')
+        fp.flush()
+
+    def reset(self):
+        """ Resets the panel by chaining the following operations :
+
+        * clear the display
+        * turns the back light on
+        * turns the cursor display off
+        * turns all the keypad LEDs off
+        * set the keypad scanner of the LCD controller in fast mode
+        """
+        self.clear()
+        self.set_backlight(True)
+        self.set_leds_state(0)
+
+    def clear(self):
+        fp = self._fp(self.F_DISPLAY)
+        fp.write('\x0c')
+        fp.flush()
+
+    def write_at(self, s, line=1, col=1):
+        """ Convenience method to write a text at a given location.
+
+        Arguments:
+            s:
+                the text
+            line, col:
+                the text position
+        """
+        self.write("\x1b[%d;%dH%s" % (line, col, s))
+
+    def write(self, s):
+        fp = self._fp(self.F_DISPLAY)
+        fp.write(s)
+        fp.flush()
+
+    def get_keypad_state(self):
+        return int(self._fp(self.F_KEYS).read())
