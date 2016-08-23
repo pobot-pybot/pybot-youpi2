288678dfa34ef19bc814e10356e3b73018e4a601
Eric Pascual <eric@pobot.org>
2016-08-23 23:32:23 +0200

    main mechanism revisited

--- src/pybot/youpi2/app.py
+++ src/pybot/youpi2/app.py
@@ -2,8 +2,11 @@
 
 import sys
 import signal
+import os
+import logging.config
 
 from pybot.core import cli
+from pybot.core import log
 
 from pybot.youpi2.ctlpanel import ControlPanel
 from pybot.youpi2.ctlpanel.devices.fs import FileSystemDevice
@@ -12,27 +15,39 @@ __author__ = 'Eric Pascual'
 
 
 class YoupiApplication(object):
+    NAME = 'app'
     TITLE = "Youpi application"
 
-    @staticmethod
-    def main():
-        app = YoupiApplication()
+    def __init__(self):
+        log_cfg = log.get_logging_configuration({
+            'handlers': {
+                'file': {
+                    'filename': os.path.expanduser('~/youpi2-%s.log' % self.NAME)
+                }
+            },
+            'root': {
+                'handlers': ['file']
+            }
+        })
+        logging.config.dictConfig(log_cfg)
+        self.logger = log.getLogger(self.__class__.__name__)
 
+        self.pnl = None
+        self.terminated = False
+
+    def main(self):
         parser = cli.get_argument_parser()
         parser.add_argument('--pnldev', default="/mnt/lcdfs")
         parser.add_argument('--armdev', default="/mnt/y2fs")
 
-        app.add_custom_arguments(parser)
-        sys.exit(app.run(parser.parse_args()))
-
-    def __init__(self):
-        self.pnl = None
-        self.terminated = False
+        self.add_custom_arguments(parser)
+        sys.exit(self.run(parser.parse_args()))
 
     def terminate(self, *args):
         self.terminated = True
 
     def run(self, args):
+        self.logger.info('creating control panel device (path=%s)', args.pnldev)
         self.pnl = ControlPanel(FileSystemDevice(args.pnldev))
 
         signal.signal(signal.SIGTERM, self.terminate)
@@ -41,21 +56,27 @@ class YoupiApplication(object):
         self.pnl.center_text_at(self.TITLE, line=1)
 
         try:
+            self.logger.info('invoking setup')
             self.setup()
         except Exception as e:
+            self.logger.error(e)
             self.run_error(e)
             return 1
 
         exit_code = 0
         try:
+            self.logger.info('starting loop')
             while not self.terminated:
                 self.loop()
         except Exception as e:
+            self.logger.error(e)
             self.unexpected_error(e)
             exit_code = 1
         finally:
+            self.logger.info('invoking teardown with exit_code=%s', exit_code)
             self.teardown(exit_code)
 
+        self.logger.info('returning with exit_code=%s', exit_code)
         return exit_code
 
     def add_custom_arguments(self, parser):
